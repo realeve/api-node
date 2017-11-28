f0f749b84afbc524903f866f8e85c3703096240f
realeve@qq.com <realeve@qq.com>
2017-11-28 14:26:01 +0800

    add gzip-middleware; update eslint settings

--- config/config.default.js
+++ config/config.default.js
@@ -7,7 +7,10 @@ module.exports = appInfo => {
     config.keys = appInfo.name + '_1511836451261_6421';
 
     // add your config here
-    config.middleware = ['response'];
+    config.middleware = ['response', 'gzip'];
 
+    config.gzip = {
+        threshold: 1024,
+    }
     return config;
 };
